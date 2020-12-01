b996ada2bfba455ac5332d129f4f77e26aa25f0f
nisha <nishay0300@gmail.com>
2020-12-01 21:17:13 +0530

    View Binding added

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -10,23 +10,67 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.twoactivities.databinding.ActivityMainBinding;
+import com.example.twoactivities.databinding.ActivitySecondBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
-    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.Message";
     public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        Log.d(LOG_TAG, "onCreate");
+
+        if (savedInstanceState != null) {
+            boolean isVisible =
+                    savedInstanceState.getBoolean("REPLY_VISIBLE");
+            // Show both the header and the message views. If isVisible is
+            // false or missing from the bundle, use the default layout.
+            if (isVisible) {
+                binding.textHeaderReply.setVisibility(View.VISIBLE);
+                binding.textMessageReply.setText(savedInstanceState
+                        .getString("REPLY_TEXT"));
+                binding.textMessageReply.setVisibility(View.VISIBLE);
+            }
+        }
+
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     @Override
@@ -35,10 +79,13 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == TEXT_REQUEST) {
             if (resultCode == RESULT_OK) {
                 String reply =
-                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
+                        null;
+                if (data != null) {
+                    reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                }
+                binding.textHeaderReply.setVisibility(View.VISIBLE);
+                binding.textMessageReply.setText(reply);
+                binding.textMessageReply.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -47,10 +94,19 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG,"Button clicked");
 
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
+        String message = binding.editTextMain.getText().toString();
         intent.putExtra(EXTRA_MESSAGE,message);
         startActivityForResult(intent, TEXT_REQUEST);
 
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (binding.textHeaderReply.getVisibility() == View.VISIBLE){
+            outState.putBoolean("REPLY_VISIBLE",true);
+            outState.putString("REPLY_TEXT",binding.textMessageReply.getText().toString());
+        }
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,31 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.twoactivities.databinding.ActivitySecondBinding;
+
 import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
-    private EditText mReply;
+    public static final String EXTRA_REPLY = "com.example.twoactivities.extra.Reply";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+    private ActivitySecondBinding b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
+        b = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+
+        Log.d(LOG_TAG, "onCreateSecond");
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+        b.textMessage.setText(message);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStartSecond");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestartSecond");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPauseSecond");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStopSecond");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroySecond");
+    }
+
+
     public void returnReply(View view) {
-        String reply = mReply.getText().toString();
-        Intent replyIntent = new Intent();
+        String reply = b.editTextSecond.getText().toString();
+        Intent replyIntent = new Intent(this,MainActivity.class);
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
         finish();
